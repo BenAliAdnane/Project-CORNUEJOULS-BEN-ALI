0b10a30953e66f96a45e904ba917e72e235aa466
BenAliAdnane <adnane.ben-ali@univ-tlse3.fr>
2021-11-27 12:08:38 +0100

    Premier test du git commit

---
+++ fonction_base-v1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    printf("hello there !");
+    scanf("your age is %d", &age);
+    printf("hi");
+    printf("%d", age );
+    return 0;
+
+}
