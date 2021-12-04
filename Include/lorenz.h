b91b85d261d10ae537f92c24c1dad4b90fe97ed3
Damien Cornuejouls <damien.cornuejouls@univ-tlse3.fr>
2021-12-04 17:37:01 +0100

    Premiere version de Lorenz

--- Include/lorenz.h
+++ Include/lorenz.h
@@ -3,3 +3,9 @@
 #include <ctype.h>
 
 void pos_initiale(float *t);
+
+void def_Pas(float *t);
+
+void def_settings(int *sigma, int* rho, int* beta);
+
+int * point_suivant(int *t, int sigma, int rho, int beta);
