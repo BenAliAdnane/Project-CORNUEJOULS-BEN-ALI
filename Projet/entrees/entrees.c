6adcb5c87eb71f2cc04920f2b909efe978436d18
BenAliAdnane <adnane.ben-ali@univ-tlse3.fr>
2021-12-09 21:37:14 +0100

    qlq chgmt

--- Projet/entrees/entrees.c
+++ Projet/entrees/entrees.c
@@ -5,86 +5,36 @@
 #include <string.h>
 #define TAILLE_MAX 10
 
-struct chaine
+int lire_fin_ligne()
 {
-    char * t;
-};
-
-struct chaine * nouvelle_chaine(int nb_valeur , char * filename)
-{
-    struct chaine * res;
-    FILE* file = fopen(filename,"w+");
-    char caractere_actuel = fgetc(file);
-    res = (struct chaine *)malloc(sizeof(struct chaine));
-    res->t = (char *)malloc(nb_valeur*(sizeof(char)));
-    for (int i=0 ; caractere_actuel!=EOF; i++)
+    int compteur = 0;
+    char caractere = getchar();
+    while(caractere!=EOF && caractere!='\n')
     {
-        if(!isspace(caractere_actuel))
-            res->t[i]=caractere_actuel;
-        caractere_actuel=fgetc(file);
+        if(!isspace(caractere))
+            compteur+=1;
+        caractere= getchar();
     }
-    return res;
+    return compteur;
 }
 
-int lit_format(char * filename, int nb_valeur)  // renvoie 1 si que des entiers ou décimaux, 0 sinon
+void lire_entier(int * n)
 {
-    struct chaine * echange;
-    echange = nouvelle_chaine(nb_valeur, filename);
-
-    int a = strlen(echange->t);
-
-    for(int i=0; i<a; i++)
-    {
-        if(!isdigit(echange->t[i]))
-            return 0;
-    }
-    return 1;
-} 
-
-int lire_fin_ligne() // indique le nombre de caractère qu'il reste dans une ligne //
-{
-    int cpt = 0;
-    char c = getchar(); // renvoie directement ce qu'il y'a dans le buffer char par char ou EOF si fin ligne//
-    while(c!=EOF && c!='\n')
-    {
-        if(!isspace(c))
-            cpt+=1;
-        c= getchar();
-    }
-    return cpt;
+    lit_format("%d", n);
 }
 
-void lire_entier(char * filename,int nb_valeur)
+void lire_decimal(int * n)
 {
-    do 
-    {
-        if(lit_format(filename,nb_valeur)==0)
-            printf("refais");
-    }while(lit_format(filename, nb_valeur)!=1);
+    lit_format("%f", n);
 }
 
-void lire_decimal( char * filename , int nb_valeur) // Au final ce sont les même, mais on aura besoin que de lire_entier
+void lit_format(char * format, int * n)
 {
-    if(lit_format(filename, nb_valeur)==0)
-        printf("refais");
-}
-
-void demande_dt()
-{
-    
-    //tu crées en avance un fichier dt //
-    FILE* file;
-    float a;
-    printf("hey");
+    int lu = 0;
+    int nombre;
     do
     {
-        file = fopen("demande_dt.txt","w+");
-        printf("%d",lit_format("demande_dt.txt",10));
-        scanf("%f",&a);
-        if ( lit_format("demande_dt.txt",10)==1)
-            fputc(a,file);
-        else
-            fclose(file);
-    } while(lit_format("demande_dt.txt",10)!=1);
+        lu = scanf(format, n);
+        nombre = lire_fin_ligne();
+    }while((lu!=1) || (nombre > 0));
 }
-
